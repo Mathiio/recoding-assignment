5f4af14cbcfece274e4594ddd0fc39749df201d3
Mathiio <matheo.pougalan@gmail.com>
2023-03-20 13:55:43 +0100

    fix : V1 noising

--- V1/V2.js
+++ V1/V2.js
@@ -23,24 +23,39 @@ function draw() {}
 //Prend 5 arguments : coordonnées (x,y) du coin supérieur gauche du carré, largeur carré, hauteur carré, niveau de subdivision.
 function drawSquare(x, y, w, h, level) {
   //Si le niveau de subdivision est inférieur à 7 alors on continue de dessiner des carrés en subdivisant
-  if (level < 7) {
+  if (level < 3.5) {
     //Calcule la moitié de la largeur et de la hauteur du carré actuel
-    let subW = w / 2;
-    let subH = h / 2;
-    //Dessine quatres nouveaux carrés quatre fois plus petit que le carré actuela avec l'appel de la fonction drawSquare
+    let subW = w / 4;
+    let subH = h / 4;
+    //Dessine seize nouveaux carrés quatre fois plus petit que le carré actuela avec l'appel de la fonction drawSquare
     //Incrémentation de la variable du niveau de subdivision de 1, car il y a eu une subdivision du carré actuel
     drawSquare(x, y, subW, subH, level + 1);
     drawSquare(x + subW, y, subW, subH, level + 1);
-    drawSquare(x + subW, y + subH, subW, subH, level + 1);
+    drawSquare(x + subW + subW, y, subW, subH, level + 1);
+    drawSquare(x + subW + subW + subW, y, subW, subH, level + 1);
+
     drawSquare(x, y + subH, subW, subH, level + 1);
+    drawSquare(x + subW, y + subH, subW, subH, level + 1);
+    drawSquare(x + subW + subW, y + subH, subW, subH, level + 1);
+    drawSquare(x + subW + subW + subW, y + subH, subW, subH, level + 1);
+
+    drawSquare(x, y + subH + subH, subW, subH, level + 1);
+    drawSquare(x + subW, y + subH + subH, subW, subH, level + 1);
+    drawSquare(x + subW + subW, y + subH + subH, subW, subH, level + 1);
+    drawSquare(x + subW + subW + subW, y + subH + subH, subW, subH, level + 1);
+
+    drawSquare(x, y + subH + subH + subH, subW, subH, level + 1);
+    drawSquare(x + subW, y + subH + subH + subH, subW, subH, level + 1);
+    drawSquare(x + subW + subW, y + subH + subH + subH, subW, subH, level + 1);
+    drawSquare(x + subW + subW + subW, y + subH + subH + subH, subW, subH, level + 1);
   }
     //Une fois le niveau de subdivision atteint
     else {
     //"noiseval" génère une valeur de bruit pour les coordonnées x et y du coin supérieur gauche du carré actuel.
-    let noiseVal = noise(x / 100, y / 100);
+    let noiseVal = noise(x / 50, y / 20);
     //Utilisation de cette valeur de bruit pour choisir une nuance de gris à partir du tableau GRAY_SCALE
     //Détermine si la nuance de gris sélectionnée sera la nuance la plus claire ou la plus sombre dans le tableau.
-    let grayIndex = Math.random() < 0.4 + noiseVal * 0.4 ? 3 : Math.floor(random(3));
+    let grayIndex = Math.random() < 0.05 + noiseVal * 0.8 ? 3 : Math.floor(random(3));
     //Rremplit le carré avec cette couleur en utilisant la fonction fill()
     fill(color(...GRAY_SCALE[grayIndex]));
     //Dessine un rectangle sans contour avec noStroke()
@@ -48,4 +63,4 @@ function drawSquare(x, y, w, h, level) {
     //Dessine le carré avec la fonction rect()
     rect(x, y, w, h);
   }
-}
+} 
